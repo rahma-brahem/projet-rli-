fb6bdbf21b97f08e4e86ebe08ba37c56cb1ce12c
Rahma <rahmabrahem8@gmail.com>
2024-05-03 00:57:55 +0100

    chatbot commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import './App.css';
+import ChatBot from 'react-simple-chatbot';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
-import './App.css';
-
 import { useStateContext } from './contexts/ContextProvider';
 
 const App = () => {
@@ -38,7 +38,6 @@ const App = () => {
               >
                 <FiSettings />
               </button>
-
             </TooltipComponent>
           </div>
           {activeMenu ? (
@@ -95,6 +94,51 @@ const App = () => {
           </div>
         </div>
       </BrowserRouter>
+
+      {/* Intégration du chatbot */}
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}, nice to meet you!',
+            trigger: '4',
+          },
+          {
+            id: '4',
+            message: 'What number am I thinking?',
+            trigger: '5',
+          },
+          {
+            id: '5',
+            options: [
+              { value: 1, label: 'Number 1', trigger: '7' },
+              { value: 2, label: 'Number 2', trigger: '6' },
+              { value: 3, label: 'Number 3', trigger: '6' },
+            ],
+          },
+          {
+            id: '6',
+            message: 'Wrong answer, try again.',
+            trigger: '5',
+          },
+          {
+            id: '7',
+            message: 'Awesome! You are a telepath!',
+            end: true,
+          },
+        ]}
+        floating={true}
+      />
     </div>
   );
 };
